53b0b6015753a3d0dd3e2538987f1bb0b9103925
Tyler Blease <saebeldesigns@gmail.com>
2022-12-23 15:47:39 -0800

    added nodemon to root

--- backend/server.js
+++ backend/server.js
@@ -17,15 +17,15 @@ app.listen(port, () => {
   connectDb();
 });
 
-const __dirname = path.resolve();
-// const dir = path.resolve();
-// const __dirname = dir.slice(0, -8);
+// const __dirname = path.resolve();
+const dir = path.resolve();
+const __dirname = dir.slice(0, -8); //remove /backend from pathname
 console.log(__dirname);
 
-// app.use(express.static(path.join(__dirname, 'frontend/build')));
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
-// );
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
+);
 
 // USERS
 app.get('/api/users', async (req, res) => {
