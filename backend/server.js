99efc1a77cca278e2b70526316ec4827c7f37b9c
Tyler Blease <saebeldesigns@gmail.com>
2022-12-23 18:50:04 -0800

    commented out path

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,11 @@ app.listen(port, () => {
 });
 
 // const __dirname = path.resolve();
-const dir = path.resolve();
-const __dirname = dir.slice(0, -8); //remove /backend from pathname
-console.log(__dirname);
+// const dir = path.resolve();
+// const __dirname = dir.slice(0, -8); //remove /backend from pathname
+// console.log(__dirname);
 
-app.use(express.static(path.join(__dirname, './frontend/build')));
+// app.use(express.static(path.join(__dirname, './frontend/build')));
 // app.get('/api', (req, res) =>
 //   res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 // );
