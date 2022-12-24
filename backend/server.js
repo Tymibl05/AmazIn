f26751080c7f23bec20a6f1b2e26573d4ee9e4fc
Tyler Blease <saebeldesigns@gmail.com>
2022-12-23 18:15:06 -0800

    commented app.get(/api)

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,9 @@ const __dirname = dir.slice(0, -8); //remove /backend from pathname
 console.log(__dirname);
 
 app.use(express.static(path.join(__dirname, './frontend/build')));
-app.get('/api', (req, res) =>
-  res.sendFile(path.join(__dirname, './frontend/build/index.html'))
-);
+// app.get('/api', (req, res) =>
+//   res.sendFile(path.join(__dirname, './frontend/build/index.html'))
+// );
 
 // USERS
 app.get('/api/users', async (req, res) => {
